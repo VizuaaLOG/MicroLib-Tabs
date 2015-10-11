f3e2b46181c3ab3313a1a8395ab51817656bb0d1
Thomas Erbe <vizuaalog@gmail.com>
2015-10-11 16:15:39 +0100

    Add versioning to files so there is a archive of past versions

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require("gulp"),
     uglify = require("gulp-uglify"),
     rename = require("gulp-rename");
 
+var version = "1.0.0";
+
 gulp.task("minify", function() {
-    return gulp.src("./src/tabs.microlib.js")
+    return gulp.src("./src/tabs.microlib-"+version+".js")
         .pipe(uglify())
         .pipe(rename( {suffix: ".min"} ))
         .pipe(gulp.dest("./dist/"));
